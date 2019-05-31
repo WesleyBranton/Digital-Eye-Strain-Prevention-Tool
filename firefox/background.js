b71829cb15b800ab9c29d191a2ce3cdb14517594
Wesley Branton <addons@wesleybranton.com>
2019-05-31 17:10:00 -0400

    Added license headers

--- firefox/background.js
+++ firefox/background.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 // Check if user has set a notification type
 browser.storage.local.get('notificationMode', (res) => {
 	if (typeof res.notificationMode === 'undefined') {
