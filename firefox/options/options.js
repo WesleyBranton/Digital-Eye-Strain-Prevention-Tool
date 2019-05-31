203958b82ca1eddc0e9ae69e4a18a022ca3d1332
Wesley Branton <addons@wesleybranton.com>
2019-05-31 14:45:28 -0400

    Added comments

--- firefox/options/options.js
+++ firefox/options/options.js
@@ -1,10 +1,14 @@
 // Save options
 function saveOptions() {
+	// Notification mode setting
 	var setting = document.getElementById('notificationMode').value;
 	browser.storage.local.set({'notificationMode': setting});
+	
+	// Do Not Disturb setting
 	var disable = document.getElementById('tempDisabled').value;
 	browser.storage.local.set({'tempDisabled': disable});
 	
+	// Apply changes to Do Not Disturb setting
 	if (disable == 1 && !isDisabled) {
 		chrome.runtime.sendMessage("disabletimer");
 		isDisabled = true;
@@ -16,10 +20,12 @@ function saveOptions() {
 
 // Prefill saved settings into option page
 function restoreOptions() {
+	// Notification mode setting
 	browser.storage.local.get('notificationMode', (res) => {
 		var setting = document.getElementById('notificationMode');
 		setting.value = res.notificationMode;
 	});
+	// Do Not Disturb setting
 	browser.storage.local.get('tempDisabled', (res) => {
 		var setting = document.getElementById('tempDisabled');
 		setting.value = res.tempDisabled;
